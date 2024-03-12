003a7ada2a3f8be6ff8642983abae3ee03df0cad
denisbelenko <denisdel123@>
2024-03-12 21:27:53 +0700

    добавил CRUD для сущности dog так же добавил категорию в сущность dog создал шаблоны

--- zooApp/views.py
+++ zooApp/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 
-from zooApp.models import User, Category
+from zooApp.models import User, Category, Dog
 
 
 class CategoryListView(ListView):
     model = Category
 
 
-class MainListView(ListView):
-    model = Category
-    template_name = 'zooApp/main_list.html'
+def main(request):
+    context = {
+        'title': 'Главная страница'
+    }
+    return render(request, 'zooApp/main_list.html', context)
+
+
+class DogsListView(ListView):
+    model = Dog
+
+
+class DogDetailView(DetailView):
+    model = Dog
+
+
+class DogCreateView(CreateView):
+    model = Dog
+    fields = ('name', 'age', 'description', 'photo', 'price', 'category')
+
+    success_url = reverse_lazy('zooApp:category')
+
+
+class DogUpdateView(UpdateView):
+    model = Dog
+    fields = ('name', 'age', 'description', 'photo', 'price', 'category')
+    success_url = reverse_lazy('zooApp:category')
+
+
+class DogDeleteView(DeleteView):
+    model = Dog
+    success_url = reverse_lazy('zooApp:category')
