ea7565416cc0e1af09a5e243f39268199a2ee8a7
denisbelenko <denisdel123@>
2024-03-13 20:03:13 +0700

    создал CRU для сущности блог так же исправил шаблоны и добавил несколько новых шаблонов

--- zooApp/urls.py
+++ zooApp/urls.py
@@ -1,24 +1,33 @@
 from django.urls import path
 
 from zooApp.apps import ZooappConfig
-from zooApp.views import CategoryListView, main, DogsListView, DogDetailView, DogUpdateView, DogCreateView, \
-    DogDeleteView, CategoryCreateView, CategoryUpdateView, settings
+from zooApp.views import CategoryListView, DogsListView, DogDetailView, DogUpdateView, DogCreateView, \
+    DogDeleteView, CategoryCreateView, CategoryUpdateView, settings, CategoryDeleteView, BlogCreateView, BlogListView, \
+    BlogUpdateView, BlogDetailView
 
 app_name = ZooappConfig.name
 
 urlpatterns = [
-    path('', main, name='main'),
+    path('', BlogListView.as_view(), name='main'),
     path('settings/', settings, name='settings'),
 
 
     path('category/', CategoryListView.as_view(), name='category'),
-    path('createcategory/', CategoryCreateView.as_view(), name='createcategory'),
-    path('<int:pk>updatecategory/', CategoryUpdateView.as_view(), name='updatecategory'),
+    path('category/create/', CategoryCreateView.as_view(), name='createcategory'),
+    path('<int:pk>category/update/', CategoryUpdateView.as_view(), name='updatecategory'),
+    path('<int:pk>category/delete/', CategoryDeleteView.as_view(), name='deletecategory'),
 
-    path('createdog/', DogCreateView.as_view(), name='createdog'),
+    path('create/dog/', DogCreateView.as_view(), name='createdog'),
     path('<int:pk>dogs/', DogsListView.as_view(), name='dogs'),
-    path('<int:pk>dogdetail/', DogDetailView.as_view(), name='dogdetail'),
-    path('<int:pk>updatedog/', DogUpdateView.as_view(), name='updatedog'),
-    path('<int:pk>deletedog/', DogDeleteView.as_view(), name='deletedog'),
+    path('<int:pk>dog/detail/', DogDetailView.as_view(), name='dogdetail'),
+    path('<int:pk>dog/update/', DogUpdateView.as_view(), name='updatedog'),
+    path('<int:pk>dog/delete/', DogDeleteView.as_view(), name='deletedog'),
+
+    path('blog/create/', BlogCreateView.as_view(), name='createblog'),
+    path('<int:pk>blog/update/', BlogUpdateView.as_view(), name='updateblog'),
+    path('<int:pk>blog/detail/', BlogDetailView.as_view(), name='detailblog'),
+
+
+
 
 ]
--- zooApp/views.py
+++ zooApp/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 
-from zooApp.models import User, Category, Dog
+from zooApp.models import User, Category, Dog, Blog
 
 
 class CategoryListView(ListView):
     model = Category
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Породы'
+        return context
 
 
 class CategoryCreateView(CreateView):
@@ -21,11 +26,9 @@ class CategoryUpdateView(UpdateView):
     success_url = reverse_lazy('zooApp:category')
 
 
-def main(request):
-    context = {
-        'title': 'Главная страница'
-    }
-    return render(request, 'zooApp/main_list.html', context)
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy('zooApp:category')
 
 
 def settings(request):
@@ -37,6 +40,11 @@ def settings(request):
 
 class DogsListView(ListView):
     model = Dog
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Собаки'
+        return context
 
     def get_queryset(self):
         category_pk = self.kwargs['pk']
@@ -63,3 +71,34 @@ class DogUpdateView(UpdateView):
 class DogDeleteView(DeleteView):
     model = Dog
     success_url = reverse_lazy('zooApp:category')
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'content', 'photo',)
+    success_url = reverse_lazy('zooApp:main')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'photo',)
+    success_url = reverse_lazy('zooApp:main')
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'zooApp/main_list.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная Страница'
+        return context
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Публикация'
+        return context
