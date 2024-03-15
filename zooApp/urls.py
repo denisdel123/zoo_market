569fb72982ba8f025f88090a8c24829b9a0039af
denisbelenko <denisdel123@>
2024-03-15 12:21:15 +0700

    добавил ограничение карточек на каждой странице и редактировал название ссылок по PEP8

--- zooApp/urls.py
+++ zooApp/urls.py
@@ -12,21 +12,21 @@ urlpatterns = [
     path('settings/', settings, name='settings'),
 
     path('category/', CategoryListView.as_view(), name='category'),
-    path('category/create/', CategoryCreateView.as_view(), name='createcategory'),
-    path('<int:pk>category/update/', CategoryUpdateView.as_view(), name='updatecategory'),
-    path('<int:pk>category/delete/', CategoryDeleteView.as_view(), name='deletecategory'),
+    path('category/create/', CategoryCreateView.as_view(), name='create_category'),
+    path('<int:pk>category/update/', CategoryUpdateView.as_view(), name='update_category'),
+    path('<int:pk>category/delete/', CategoryDeleteView.as_view(), name='delete_category'),
 
-    path('create/dog/', DogCreateView.as_view(), name='createdog'),
+    path('create/dog/', DogCreateView.as_view(), name='create_dog'),
     path('<int:pk>dogs/', DogsListView.as_view(), name='dogs'),
-    path('<int:pk>dog/detail/', DogDetailView.as_view(), name='dogdetail'),
-    path('<int:pk>dog/update/', DogUpdateView.as_view(), name='updatedog'),
-    path('<int:pk>dog/delete/', DogDeleteView.as_view(), name='deletedog'),
+    path('<int:pk>dog/detail/', DogDetailView.as_view(), name='dog_detail'),
+    path('<int:pk>dog/update/', DogUpdateView.as_view(), name='update_dog'),
+    path('<int:pk>dog/delete/', DogDeleteView.as_view(), name='delete_dog'),
 
     path('', BlogListView.as_view(), name='main'),
     path('unpublished', BlogUnpublishedListView.as_view(), name='unpublished'),
-    path('blog/create/', BlogCreateView.as_view(), name='createblog'),
-    path('<int:pk>blog/update/', BlogUpdateView.as_view(), name='updateblog'),
-    path('<int:pk>blog/detail/', BlogDetailView.as_view(), name='detailblog'),
-    path('<int:pk>blog/delete/', BlogDeleteView.as_view(), name='deleteblog'),
+    path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
+    path('<int:pk>blog/update/', BlogUpdateView.as_view(), name='update_blog'),
+    path('<int:pk>blog/detail/', BlogDetailView.as_view(), name='detail_blog'),
+    path('<int:pk>blog/delete/', BlogDeleteView.as_view(), name='delete_blog'),
 
 ]
